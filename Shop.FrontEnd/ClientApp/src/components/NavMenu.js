768d753f42897e4206804bd6ff75eaf27fc63a06
Sergiy Kuzmich <nika246458246459@gmail.com>
2019-08-16 17:58:46 +0300

    start

--- Shop.FrontEnd/ClientApp/src/components/NavMenu.js
+++ Shop.FrontEnd/ClientApp/src/components/NavMenu.js
@@ -1,7 +1,6 @@
 ﻿import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -18,21 +17,7 @@ export class NavMenu extends Component {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
-            <LinkContainer to={'/'} exact>
-              <NavItem>
-                <Glyphicon glyph='home' /> Home
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/counter'}>
-              <NavItem>
-                <Glyphicon glyph='education' /> Counter
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/fetchdata'}>
-              <NavItem>
-                <Glyphicon glyph='th-list' /> Fetch data
-              </NavItem>
-            </LinkContainer>
+           
           </Nav>
         </Navbar.Collapse>
       </Navbar>
