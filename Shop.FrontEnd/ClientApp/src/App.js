7fc32700aff15f8a113df2656b89bb6c11baefe9
Sergiy Kuzmich <nika246458246459@gmail.com>
2019-08-24 03:51:06 +0300

    add cart page and fill her

--- Shop.FrontEnd/ClientApp/src/App.js
+++ Shop.FrontEnd/ClientApp/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import  Layout from './components/Layout';
-import  Home from './components/pages/Home/Home';
-import  Service from './components/pages/Service/Service'
-import  SignUp from './components/pages/SignUp/SignUp'
+import Layout from './components/Layout';
+import Home from './components/pages/Home/Home';
+import Service from './components/pages/Service/Service'
+import SignUp from './components/pages/SignUp/SignUp'
 import SignIn from './components/pages/SignIn/SignIn'
+import Cart from './components/pages/Cart/Cart'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 export default () => (
   <Layout>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/services' component={Service} />
-      <Route path='/user/signup' component={SignUp }/>
-      <Route path='/user/signin' component={SignIn }/>
+      <Route exact path='/' component={ Home } />
+      <Route exact path='/services' component={ Service} />
+      <Route exact path='/user/signup' component={ SignUp }/>
+      <Route exact path='/user/signin' component={ SignIn }/>
+      <Route exact path='/cart' component={ Cart }/>
   </Layout>
 );
