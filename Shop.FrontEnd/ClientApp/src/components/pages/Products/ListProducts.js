fbf40da3c70f2f5a74547dccf7b877577052cebe
Sergiy Kuzmich <nika246458246459@gmail.com>
2019-09-09 01:37:01 +0300

    update filter

--- Shop.FrontEnd/ClientApp/src/components/pages/Products/ListProducts.js
+++ Shop.FrontEnd/ClientApp/src/components/pages/Products/ListProducts.js
@@ -13,37 +13,24 @@ class ListProducts extends Component {
             sizeTable: ['XS', 'S', 'M', 'L', 'XL', 'XXL', '36', '36.5', '37', '37.5', '38',
                 '38.5', '39', '39.5', '40', '40.5', '41', '41.5', '42', '42.5',
                 '43', '43.5', '44', '44.5', '45', '45.5', '46', '46.5', '47'],
-               slidervalue: { min: 115, max: 3700 }
+            slidervalue: { min: 125, max: 3700 },
+            targetElemId: ''
         };
     }
     componentDidMount() {
         window.addEventListener('scroll', this.scroll);
+        
     }
     createSizeTable(value, index) {
-            return (
-                <Col lg={4}>
-                    <Link to={`size=${value}`}>
-                        <li className='secondary'>
-                            <span>{value}</span>
-                        </li>
-                    </Link>
-                </Col>
-            );
-    }
-    scroll() {
-        ///NEED FIX!!!
-        try {
-            let classNameFilter = 'filter';
-            const element = document.getElementById('filter');
-            if (window.pageYOffset >= 300) {
-                element.setAttribute('class', 'filter fixed')
-            }
-            else {
-                element.setAttribute('class', 'filter')
-            }
-        }
-        catch (ex) { console.warn(ex) }
-        ///NEED FIX!!!
+        return (
+            <Col lg={4}>
+                <Link to={`size=${value}`}>
+                    <li className='secondary'>
+                        <span>{value}</span>
+                    </li>
+                </Link>
+            </Col>
+        );
     }
     clickAnimation = (e) => {
         const element = e.target;
@@ -58,12 +45,32 @@ class ListProducts extends Component {
             te.setAttribute('class', 'hidden');
         }
     }
+    changePrice() {
+        const min = document.getElementById('min').value;
+        const max = document.getElementById('max').value;
+        this.setState(prevState => {
+            let slidervalue = Object.assign({}, prevState.slidervalue);
+            if (isNaN(min)) {
+                slidervalue.min = 0;
+            }
+            else {
+                slidervalue.min = parseInt(min);
+            }
+            if (isNaN(max)) {
+                slidervalue.max = 0;
+            }
+            else {
+                slidervalue.max = parseInt(max);
+            }
+            return { slidervalue };
+        })
+    }
+
     render() {
-        console.log(this.state);
         return (
             <div className='list-products'>
                 <Row>
-                    <Col lg={2}>
+                    <Col lg={2} >
                         <div id='filter' className='filter'>
                             <ul>
                                 <li className='title'>
@@ -71,20 +78,23 @@ class ListProducts extends Component {
                                 </li>
                                 <li className='main'>
                                     <button className='hide-filter' id='gender' onClick={(e) => this.clickAnimation(e)}><span>СТАТЬ</span></button>
-                                    <ul className='visible' id='gender-menu'>
-                                        <li className='secondary'>
+                                    <ul className='hidden' id='gender-menu'>
+                                        <li className='secondary'><Link to='/catalog?gender=man'>
                                             <input type='checkbox' id='man' />
-                                            <span>Чоловіча</span>
+                                            <span>Чоловіча</span></Link>
                                         </li>
+                                        
+                                        <Link to='/catalog?gender=woman'>
                                         <li className='secondary'>
                                             <input type='checkbox' id='woman' />
                                             <span>Жіноча</span>
                                         </li>
+                                        </Link>
                                     </ul>
                                 </li>
                                 <li className='main'>
                                     <button className='hide-filter' id='category' onClick={(e) => this.clickAnimation(e)}><span id='category'>КАТЕГОРІЯ</span></button>
-                                    <ul className='visible' id='category-menu'>
+                                    <ul className='hidden' id='category-menu'>
                                         <li className='secondary'>
                                             <input type='checkbox' id='weather' />
                                             <span>Одежа</span>
@@ -101,7 +111,7 @@ class ListProducts extends Component {
                                 </li>
                                 <li className='main'>
                                     <button className='hide-filter' id='brand' onClick={(e) => this.clickAnimation(e)}><span>БРЕНД</span></button>
-                                    <ul className='visible' id='brand-menu'>
+                                    <ul className='hidden' id='brand-menu'>
                                         <li className='secondary'>
                                             <input type='checkbox' id='nike' />
                                             <span>Nike</span>
@@ -138,7 +148,7 @@ class ListProducts extends Component {
                                 </li>
                                 <li className='main'>
                                     <button className='hide-filter' id='color' onClick={(e) => this.clickAnimation(e)}><span>КОЛІР</span></button>
-                                    <ul className='visible' id='color-menu'>
+                                    <ul className='hidden' id='color-menu'>
                                         <li className='secondary'>
                                             <input type='checkbox' id='black' title='чорний'
                                                 style={{ background: 'black' }} />
@@ -167,26 +177,33 @@ class ListProducts extends Component {
                                 </li>
                                 <li className='main'>
                                     <button className='hide-filter' id='size' onClick={(e) => this.clickAnimation(e)}><span>РОЗМІР</span></button>
-                                    <ul className='visible' id='size-menu'>
+                                    <ul className='hidden' id='size-menu'>
                                         <Row>
-                                        {this.state.sizeTable.map((value, index, array) =>
-                                            this.createSizeTable(value,index)
-                                        )}
+                                            {this.state.sizeTable.map((value, index, array) =>
+                                                this.createSizeTable(value, index)
+                                            )}
                                         </Row>
                                     </ul>
                                 </li>
                                 <li className='main'>
                                     <button className='hide-filter' id='price' onClick={(e) => this.clickAnimation(e)}><span>ЦІНА</span></button>
-                                    <div className='price-menu'>
+                                    <div className='price-menu hidden' id='price-menu'>
                                         <InputRange
                                             maxValue={3700}
                                             minValue={115}
-                                            step={25} 
+                                            step={25}
                                             value={this.state.slidervalue}
-                                            onChange={slidervalue => this.setState({ slidervalue })} 
-                                            id='slide'/>
-                                            <input type='text' value={'UAH ' + this.state.slidervalue.min}/>
-                                            <input type='text' value={'UAH ' + this.state.slidervalue.max}/>
+                                            onChange={slidervalue => this.setState({ slidervalue })}
+                                            id='slide' />
+                                        <input type='text' id='min' value={this.state.slidervalue.min}
+                                            onChange={() => this.changePrice()} />
+                                        <span>UAH</span>
+                                        <input type='text' id='max' value={this.state.slidervalue.max}
+                                            onChange={() => this.changePrice()} />
+                                        <span>UAH</span>
+                                        <Link to={`/&pmin=${this.state.slidervalue.min}&pmax=${this.state.slidervalue.max}`}>
+                                            <i class="fa fa-angle-right"></i>
+                                        </Link>
                                     </div>
                                 </li>
                             </ul>
