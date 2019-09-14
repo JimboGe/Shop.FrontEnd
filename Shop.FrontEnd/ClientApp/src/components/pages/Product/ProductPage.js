7cf73fdc2bd76789148eb4807df3b03d7b1f486f
Sergiy Kuzmich <nika246458246459@gmail.com>
2019-09-15 02:28:14 +0300

    update productpage

--- Shop.FrontEnd/ClientApp/src/components/pages/Product/ProductPage.js
+++ Shop.FrontEnd/ClientApp/src/components/pages/Product/ProductPage.js
@@ -10,10 +10,18 @@ class ProductPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            counter: 1
+            counter: 1,
+            arrImages: []
         };
     }
-    
+    componentWillMount() {
+        this.state.arrImages.push(
+            'https://gard.com.ua/image/cache/catalog/shop/products/54a6416c-cbc0-11e8-ab13-ee24cb1b971f-930x1240.jpg',
+            'https://gard.com.ua/image/cache/catalog/shop/products/b917841c-ce0e-11e8-ab13-ee24cb1b971f-930x1240.jpg',
+            'https://gard.com.ua/image/cache/catalog/shop/products/54a6416b-cbc0-11e8-ab13-ee24cb1b971f-930x1240.jpg',
+            'https://gard.com.ua/image/cache/catalog/shop/products/54a6416b-cbc0-11e8-ab13-ee24cb1b971f-930x1240.jpg'
+        );
+    }
     choiceRatingComment(type, e) {
         const span = document.getElementById('rating');
         let arr = span.children;
@@ -65,71 +73,60 @@ class ProductPage extends Component {
         }
 
     }
-    imageZoom(type,e){
+    imageZoom(type, e) {
         var modal = document.getElementById('modal');
-        if(type=='show'){
-        modal.style.display = 'block';
-        const elem = document.getElementById('zoom-img');
-        var src = '';
-        src = e.target.style.backgroundImage;
-        src = src.slice(5, src.length-2); //put url("");
-        elem.setAttribute('src', src);
+        if (type == 'show') {
+            modal.setAttribute('class', 'modal');
+            modal.style.display = 'block';
         }
-        if(type=='hide'){
-            modal.style.display = 'none';
+        if (type == 'hide') {
+            modal.style.display = 'block';
+            modal.setAttribute('class', ' ');
         }
     }
-    hide_show(id) {
-        let elem = document.getElementById(id);
+    hide_show(e,id) {
+        const elem = document.getElementById(id);
         if (elem.getAttribute('class') == 'hidden') {
             elem.setAttribute('class', 'visible');
+            if(e!=null)
+            e.target.setAttribute('active','active');
         }
         else {
             elem.setAttribute('class', 'hidden');
+            if(e!=null)
+            e.target.setAttribute('active','');
         }
     }
+
     render() {
         return (
             <div className='product-page container'>
-                <div id='modal' className='modal'>
-                    <img id='zoom-img' className='zoom-img' />
-                    <button onClick={()=>this.imageZoom('hide',null)} className='close-modal'>X</button>
-                </div>
-               
                 <Row>
                     <Col lg={6}>
-                        <CarouselProvider
-                            naturalSlideWidth={70}
-                            naturalSlideHeight={95}
-                            totalSlides={4}>
-                            <Slider classNameTray='test'>
-                                <Slide index={0}><ImageWithZoom onClick={(e)=>{this.imageZoom('show',e)}} src='https://gard.com.ua/image/cache/catalog/shop/products/54a6416c-cbc0-11e8-ab13-ee24cb1b971f-930x1240.jpg' />
-                                </Slide>
-                                <Slide index={1}><ImageWithZoom onClick={(e)=>{this.imageZoom('show',e)}} src='https://gard.com.ua/image/cache/catalog/shop/products/b917841c-ce0e-11e8-ab13-ee24cb1b971f-930x1240.jpg' />
-                                </Slide>
-                                <Slide index={2}><ImageWithZoom onClick={(e)=>{this.imageZoom('show',e)}} src='https://gard.com.ua/image/cache/catalog/shop/products/54a6416b-cbc0-11e8-ab13-ee24cb1b971f-930x1240.jpg' />
-                                </Slide>
-                                <Slide index={3}><ImageWithZoom onClick={(e)=>{this.imageZoom('show',e)}} src='https://gard.com.ua/image/cache/catalog/shop/products/54a6416b-cbc0-11e8-ab13-ee24cb1b971f-930x1240.jpg' />
-                                </Slide>
+                        <div id='modal' className=' '>
+                            <CarouselProvider
+                                naturalSlideWidth={70}
+                                naturalSlideHeight={95}
+                                totalSlides={4}>
+                                <Slider>
+                                    {this.state.arrImages.map((value, index, arr) =>
+                                        <Slide index={index}><ImageWithZoom onClick={(e) => { this.imageZoom('show', e) }} src={value} />
+                                        </Slide>)};
                             </Slider>
-                            <ButtonBack className='button-move back' />
-                            <ButtonNext className='button-move next' />
-                            <DotGroup className='dot-group' />
-                            <div className='dot-group-image'>
-                                <Dot slide={0} className='dot-image' >
-                                    <Image src='https://gard.com.ua/image/cache/catalog/shop/products/54a6416c-cbc0-11e8-ab13-ee24cb1b971f-930x1240.jpg'></Image>
-                                </Dot>
-                                <Dot slide={1} className='dot-image' >
-                                    <Image src='https://gard.com.ua/image/cache/catalog/shop/products/b917841c-ce0e-11e8-ab13-ee24cb1b971f-930x1240.jpg'></Image>
-                                </Dot>
-                                <Dot slide={2} className='dot-image' >
-                                    <Image src='https://gard.com.ua/image/cache/catalog/shop/products/54a6416b-cbc0-11e8-ab13-ee24cb1b971f-930x1240.jpg'></Image>
-                                </Dot>
-                                <Dot slide={3} className='dot-image' >
-                                    <Image src='https://gard.com.ua/image/cache/catalog/shop/products/54a6416b-cbc0-11e8-ab13-ee24cb1b971f-930x1240.jpg'></Image>
-                                </Dot>
-                            </div>
-                        </CarouselProvider>
+                                <ButtonBack className='button-move back' />
+                                <ButtonNext className='button-move next' />
+                                <button onClick={() => this.imageZoom('hide', null)} className='close-modal'><i class="fa fa-times"></i></button>
+                                <DotGroup className='dot-group' />
+                                <div className='dot-group-image'>
+                                    {this.state.arrImages.map((value, index, arr) =>
+                                        <Dot slide={index} className='dot-image'>
+                                            <Image
+                                                src={value}>
+                                            </Image>
+                                        </Dot>)};
+                                </div>
+                            </CarouselProvider>
+                        </div>
                     </Col>
                     <Col lg={6}>
                         <div className='content'>
@@ -146,7 +143,7 @@ class ProductPage extends Component {
                                 </span>
                             </div>
                             <div className='size-grid'>
-                                <button onClick={() => { this.hide_show('size-grid-div') }}>РОЗМІРНА СІТКА<i class="fa fa-chevron-down"></i></button>
+                                <button onClick={(e) => { this.hide_show( e,'size-grid-div' ) }}>РОЗМІРНА СІТКА</button>
                                 <div id='size-grid-div' className='hidden'><img width='100%' src='https://gard.com.ua/image/catalog/shop/STRU_aea1bb99-cb02-11e9-af8a-9e1680149fdf.jpg'></img></div>
                             </div>
                             <div className='size'>
@@ -176,7 +173,7 @@ class ProductPage extends Component {
                                 <p>Ці кеди найкраще поєднувати з штанами чінос або джинсами трохи звуженого крою.</p>
                             </div>
                             <div className='add-review'>
-                                <Button onClick={() => this.hide_show('new-review')}>ВІДГУКІВ (0)</Button>
+                                <Button onClick={() => this.hide_show(null,'new-review')}>ВІДГУКІВ (0)</Button>
                                 <div className='hidden' id='new-review'>
                                     <h3>Написати відгук</h3>
                                     <input type="text" className="form-control" placeholder="Ваше ім'я" />
@@ -207,16 +204,16 @@ class ProductPage extends Component {
                     </Col>
                 </Row>
                 <Row>
-                    <h4 style={{marginLeft:'75px',marginTop:'70px'}}>РЕКОМЕНДОВАННІ ТОВАРИ</h4>
-                    <div style={{margin:'0px 80px 0px 80px'}} >
-                    <Product src_first='https://gard.com.ua/image/cache/catalog/shop/products/790ff25c-6d88-11e9-af82-9e1680149fdf-450x600.jpg'
-                        src_second='https://gard.com.ua/image/cache/catalog/shop/products/790ff25e-6d88-11e9-af82-9e1680149fdf-450x600.jpg' />
-                    <Product src_first='https://gard.com.ua/image/cache/catalog/shop/products/ccc1c473-6cac-11e9-af82-9e1680149fdf-450x600.jpg'
-                        src_second='https://gard.com.ua/image/cache/catalog/shop/products/ccc1c474-6cac-11e9-af82-9e1680149fdf-450x600.jpg' />
-                    <Product src_first='https://gard.com.ua/image/cache/catalog/shop/products/790ff25c-6d88-11e9-af82-9e1680149fdf-450x600.jpg'
-                        src_second='https://gard.com.ua/image/cache/catalog/shop/products/790ff25e-6d88-11e9-af82-9e1680149fdf-450x600.jpg' />
-                    <Product src_first='https://gard.com.ua/image/cache/catalog/shop/products/790ff25c-6d88-11e9-af82-9e1680149fdf-450x600.jpg'
-                        src_second='https://gard.com.ua/image/cache/catalog/shop/products/790ff25e-6d88-11e9-af82-9e1680149fdf-450x600.jpg' />
+                    <h4 style={{ marginLeft: '75px', marginTop: '70px' }}>РЕКОМЕНДОВАННІ ТОВАРИ</h4>
+                    <div style={{ margin: '0px 80px 0px 80px' }} >
+                        <Product src_first='https://gard.com.ua/image/cache/catalog/shop/products/790ff25c-6d88-11e9-af82-9e1680149fdf-450x600.jpg'
+                            src_second='https://gard.com.ua/image/cache/catalog/shop/products/790ff25e-6d88-11e9-af82-9e1680149fdf-450x600.jpg' />
+                        <Product src_first='https://gard.com.ua/image/cache/catalog/shop/products/ccc1c473-6cac-11e9-af82-9e1680149fdf-450x600.jpg'
+                            src_second='https://gard.com.ua/image/cache/catalog/shop/products/ccc1c474-6cac-11e9-af82-9e1680149fdf-450x600.jpg' />
+                        <Product src_first='https://gard.com.ua/image/cache/catalog/shop/products/790ff25c-6d88-11e9-af82-9e1680149fdf-450x600.jpg'
+                            src_second='https://gard.com.ua/image/cache/catalog/shop/products/790ff25e-6d88-11e9-af82-9e1680149fdf-450x600.jpg' />
+                        <Product src_first='https://gard.com.ua/image/cache/catalog/shop/products/790ff25c-6d88-11e9-af82-9e1680149fdf-450x600.jpg'
+                            src_second='https://gard.com.ua/image/cache/catalog/shop/products/790ff25e-6d88-11e9-af82-9e1680149fdf-450x600.jpg' />
                     </div>
                 </Row>
             </div>
