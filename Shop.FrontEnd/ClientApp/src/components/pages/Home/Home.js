dc9e211f9a2e4743e3809465998039d106f84c95
Sergiy Kuzmich <nika246458246459@gmail.com>
2019-09-15 02:50:10 +0300

    change carousel from home page

--- Shop.FrontEnd/ClientApp/src/components/pages/Home/Home.js
+++ Shop.FrontEnd/ClientApp/src/components/pages/Home/Home.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
-import Carousel from 'nuka-carousel';
 import './Home.css';
 import { Link } from 'react-router-dom';
 import { Row, Col } from "react-bootstrap";
-
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup, Image } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 class Home extends Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+        arrImages: []
+    };
+}
+  componentWillMount() {
+    this.state.arrImages.push(
+      '/img/home/carousel/1big-1920x500.jpg',
+      '/img/home/carousel/43-1920x500.jpg',
+      '/img/home/carousel/banner-new-size-RU-1920x500.jpg',
+      '/img/home/carousel/ru-1920x500.jpg'
+    );
+}
   render() {
     return (
       <div>
         <div className='grid'>
           <div className='item1'>
-            <div className='carousel big ' >
-              <Carousel slidesToShow={1} cellAlign="center" speed={1000} autoplay={true} autoplayInterval={4000}
-                wrapAround={true} 
-                renderCenterLeftControls={({ previousSlide }) => (
-                  <button className='left' onClick={previousSlide} />
-                )}
-                renderCenterRightControls={({ nextSlide }) => (
-                  <button className='right' onClick={nextSlide} />
-                )}>
-                <Link to='#'><img src='/img/home/carousel/1big-1920x500.jpg' /></Link>
-                <Link to='#'><img src='/img/home/carousel/43-1920x500.jpg' /></Link>
-                <Link to='#'><img src='/img/home/carousel/banner-new-size-RU-1920x500.jpg' /></Link>
-                <Link to='#'><img src='/img/home/carousel/ru-1920x500.jpg' /></Link>
-              </Carousel>
+            <div className='carousel big' >
+               <CarouselProvider naturalSlideWidth={70} naturalSlideHeight={20} totalSlides={4}>
+                  <Slider>
+                      {this.state.arrImages.map((value, index, arr) =>
+                        <Slide index={index}><Image src={value} /></Slide>)};
+                  </Slider>
+                  <DotGroup className='dot-group' />
+               </CarouselProvider>
             </div>
           </div>
           <div className='item item2'>
@@ -58,7 +65,6 @@ class Home extends Component {
               <Link to='/cart'><img src='/img/home/shoes.jpg' /></Link>
             </div>
           </div>
-         
         </div>
       </div>
     );
