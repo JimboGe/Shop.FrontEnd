8160c9d0cde9492287eda2cb44301dc0033cf862
Sergiy Kuzmich <nika246458246459@gmail.com>
2019-09-25 01:40:38 +0300

    create jwt token and viewmodel

---
+++ Shop.FrontEnd/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Shop.FrontEnd.DAL.Entites;
+using Shop.FrontEnd.Helpers;
+using Shop.FrontEnd.ViewModels;
+
+namespace Shop.FrontEnd.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Account")]
+    public class AccountController : ControllerBase
+    {
+        readonly UserManager<DbUser> _userManager;
+        readonly SignInManager<DbUser> _signInManager;
+        public AccountController(UserManager<DbUser> userManager, SignInManager<DbUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost ("register")]
+        public async Task<IActionResult> Register([FromBody]RegisterViewModel credentials)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errrors = CustomValidator.GetErrorsByModel(ModelState);
+                return BadRequest(errrors);
+            }
+            var user = new DbUser
+            {
+                UserName = credentials.Email,
+                Email = credentials.Email,
+                PhoneNumber = credentials.PhoneNumber,
+                City = credentials.City,
+                Region = credentials.Region,
+                NumberDelivery = credentials.NumberDelivery,
+                FirstName = credentials.FirstName,
+                LastName = credentials.LastName
+            };
+
+            var result = await _userManager.CreateAsync(user, credentials.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            await _signInManager.SignInAsync(user, isPersistent: false);
+
+            return Ok(CreateToken(user));
+        }
+
+        string CreateToken(DbUser user)
+        {
+            var claims = new Claim[]
+            {
+                new Claim("id", user.Id),
+                new Claim("name", user.UserName)
+            };
+
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is the secret phrase"));
+            var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+
+            var jwt = new JwtSecurityToken(signingCredentials: signingCredentials, claims: claims);
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+    }
+}
--- Shop.FrontEnd/Startup.cs
+++ Shop.FrontEnd/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,8 +8,10 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Shop.FrontEnd.DAL;
 using Shop.FrontEnd.DAL.Entites;
+using System.Text;
 
 namespace Shop.FrontEnd
 {
@@ -30,6 +33,24 @@ namespace Shop.FrontEnd
             services.AddIdentity<DbUser, IdentityRole>()
                 .AddEntityFrameworkStores<EFDbContext>()
                 .AddDefaultTokenProviders();
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("this is the secret phrase"));
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(cfg =>
+            {
+                cfg.RequireHttpsMetadata = false;
+                cfg.SaveToken = true;
+                cfg.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    IssuerSigningKey = signingKey,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true
+                };
+            });
 
             services.Configure<IdentityOptions>(options =>
             {
